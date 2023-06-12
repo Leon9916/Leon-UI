795f0fcfb882e5bc493cb64287288a3a74536542
Leon9916 <2476288442@qq.com>
2023-06-13 01:09:00 +0800

    update：修改vite配置文件，去除硬链接、符号链接

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -4,6 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+   rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.indexOf('/node_modules/') !== -1) {
+          return id.toString().split('/node_modules/')[1].split('/')[0].toString();
+        }
+      },
+    },
+    preserveSymlinks: false // 将硬链接转换为常规文件
+   }
+  },
   css: {
     preprocessorOptions: {
       less: {
