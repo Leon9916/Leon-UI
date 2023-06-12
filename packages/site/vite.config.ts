26118bc7a51ca25bc09b67c1cc783289c1b91f27
Leon9916 <134840214+Leon9916@users.noreply.github.com>
2023-06-13 01:30:45 +0800

    Update vite.config.ts

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -6,13 +6,6 @@ export default defineConfig({
   plugins: [vue()],
   build: {
    rollupOptions: {
-    output: {
-      manualChunks(id) {
-        if (id.indexOf('/node_modules/') !== -1) {
-          return id.toString().split('/node_modules/')[1].split('/')[0].toString();
-        }
-      },
-    },
     preserveSymlinks: true // 将硬链接转换为常规文件
    }
   },
